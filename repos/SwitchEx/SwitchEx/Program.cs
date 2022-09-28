bd1998dcd4628d3861005134b0c7a6052e939f42
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-09-28 12:41:24 -0400

    First one

---
+++ repos/SwitchEx/SwitchEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SwitchEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, whats your favorite subject?");
+            var Respond = Console.ReadLine();
+
+            switch (Respond)
+            {
+                case "Math":
+                    Console.WriteLine("Thats whats up whats the square root of 748");
+                    break;
+                case "English":
+                    Console.WriteLine("Have ever read Charlottes Web?");
+                    break;
+                case "Science":
+                    Console.WriteLine("Dont cut no frogs up.");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("Thats my favorite to.");
+                    break;
+                default:
+                    Console.WriteLine("Is this a real subject");
+                    break;
+            }
+        }
+    }
+}
